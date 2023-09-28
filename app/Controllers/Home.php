3c841110b8f1a5c806f94592255c8ae0ffd607b3
Talia Desty <talia.desty21@students.unila.ac.id>
2023-09-28 22:14:08 +0700

    set-database

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,15 +4,5 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function profile(): string
 
-    $data = [
-        'nama' => $nama,
-        'kelas' => $kelas,
-        'npm' => $npm,
-    ];
-    
-    {
-        return view('profile');
-    }
 }
--- app/Views/profile.php
+++ app/Views/profile.php
@@ -1,3 +1,36 @@
-<p>Nama     : Talia</p>
-<p>Kelas    : C</p>
-<p>NPM      : 2117051017</p>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Profil</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>">
+</head>
+
+<body>
+  <!-- <p>Nama: Talia D</p>
+  <p>Kelas: C</p>
+  <p>NPM: 2117051017</p> -->
+  <img src="<?= base_url('assets/img/gambar.jpg') ?>" class="rounded-circle mx-auto d-block py-3" alt="..." width="140" height="180">
+  <table class="mx-auto">
+    <tr>
+      <td>
+        <button class="p-2 bg-secondary text-white" style="width: 300px;"><?= $nama ?></button>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <button class="p-2 mt-2 bg-secondary text-white" style="width: 300px;"><?= $npm ?></button>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <button class="p-2 mt-2 bg-secondary text-white" style="width: 300px;"><?= $kelas ?></button>
+      </td>
+    </tr>
+  </table>
+</body>
+
+</html>
